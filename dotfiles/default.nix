4f0d52022d0f62cbc3e67138abecb3050a5af761
Beaver <beavernotacat@gmail.com>
2023-12-27 19:50:31 +1000

    Pre-alpha version
    Main things done but there are a ton of work to do

---
+++ dotfiles/default.nix
@@ -0,0 +1,60 @@
+{lib, pkgs, ... }:
+{
+  # Declarative dotfiles
+  imports = [
+    <home-manager/nixos>
+    ./networking.nix
+    ./fonts.nix
+    ./docker.nix
+    ./kitty.nix
+    ./hyprland.nix
+    ./gtk.nix
+    ./waybar.nix
+    ./rofi.nix
+    ./dunst.nix
+  ];
+
+  # Screen sharing
+  xdg.portal = {
+    enable = true;
+    wlr.enable = true;
+    extraPortals = [pkgs.xdg-desktop-portal-hyprland];
+  };
+
+  # Pipewire things
+  sound.enable = true;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    jack.enable = true;
+  };
+
+
+  # User packackges in home directory
+  home-manager.users.beaver = { pkgs, ... }: {
+    home.packages = with pkgs; [ 
+      dunst
+      libnotify
+      swww
+      kitty
+      firefox
+      obsidian
+      telegram-desktop
+      discord
+    ];
+    
+    nixpkgs.config.allowUnfreePredicate = pkg:
+      builtins.elem (lib.getName pkg) [
+        "discord"
+        "obsidian"
+    ];
+
+    nixpkgs.config.permittedInsecurePackages = [
+      "electron-25.9.0"
+    ];
+  home.stateVersion = "24.05"; 
+  };
+}
